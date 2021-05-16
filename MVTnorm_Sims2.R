ca22f9a53da12f5f8d2b591ca399ec25c65bbfd2
Lee Morin <42477710+LeeMorinUCF@users.noreply.github.com>
2021-05-16 15:08:39 -0400

    Simulation for model with equal covariances

--- MVTnorm_Sims2.R
+++ MVTnorm_Sims2.R
@@ -20,8 +20,8 @@
 #   Identification and Estimation in Judicial Panel Voting"
 #   by Cameron, Morin and Paarsch
 # 
-# This initial version estimates a model without covariates
-#   and specifies the covariance matrix directly.
+# This second version estimates a model without covariates
+#   and specifies the covariance matrix as the identity matrix. 
 #   
 #
 # Dependencies:
@@ -100,49 +100,109 @@ tri_probit_gen <- function(mu, Sigma, n_cases) {
 # Define Functions for Estimation
 ##################################################
 
-tri_probit_param2vec <- function(mu, Sigma) {
+tri_probit_param2vec <- function(mu, Sigma, model_name) {
   
   # Translate trivariate probit parameters to a vector.
   # This also works for bigger systems. 
+  # Note: It ignores other parameters outside the specified model. 
   # Examples: 
-  # tri_probit_param2vec(mu = c(0,1,2), Sigma = matrix(c(3,4,5,6,7,8,9,10,11), ncol = 3))
-  # tri_probit_vec2param(param = c(0, 1, 2, 4, 5, 8))
+  # tri_probit_param2vec(c(0,1,2), NULL, 'mu_only')
+  # tri_probit_param2vec(c(0,1,2), matrix(seq(3,11), ncol = 3), 'mu_const')
+  # tri_probit_param2vec(c(0,1,2), matrix(seq(3,11), ncol = 3), 'mu_cov')
   
   
   # Start with vector of means.
   param <- mu
   
-  # Append the lower triangle of Sigma. 
-  param <- c(param, Sigma[lower.tri(Sigma, diag = FALSE)])
+  # Append the parameters from the covariance matrix, if any.
+  if (model_name == 'mu_only') {
+    # No additional parameters, Sigma is the identity matrix. 
+  } else if (model_name == 'mu_const') {
+    # The lower triangle of Sigma is a constant.
+    param <- c(param, Sigma[2, 1])
+  } else if (model_name == 'mu_cov') {
+    # The entire covariance matrix is specified.
+    # Append the lower triangle of Sigma. 
+    param <- c(param, Sigma[lower.tri(Sigma, diag = FALSE)])
+  }
+  
+  
   
   return(param)
 }
 
 
-tri_probit_vec2param <- function(param) {
+tri_probit_num_params <- function(model_name) {
   
-  # Translate trivariate probit parameters to a vector.
-  # This must have the correct number of parameters (6).
-  # Examples: 
-  # tri_probit_vec2param(param = seq(6))
-  # 
+  # Determine the number of parameters for each model.  
   
-  if (length(param) != 6) {
-    stop("param not correct length.")
+  if (model_name == 'mu_only') {
+    
+    # No additional parameters for Sigma, Sigma is the identity matrix. 
+    num_params <- 3
+    
+  } else if (model_name == 'mu_const') {
+    
+    # The lower triangle of Sigma is a constant.
+    num_params <- 4
+    
+  } else if (model_name == 'mu_cov') {
+    
+    # The entire covariance matrix is specified.
+    # Three parameters in the lower triangle. 
+    num_params <- 6
+    
   }
   
-  # Extract vector of means.
-  mu <- param[1:3]
-  param <- param[4:6]
+}
+
+
+tri_probit_vec2param <- function(param, model_name) {
   
-  # Generate covariance matrix. 
+  # Translate trivariate probit parameters to a vector.
+  # This must have the correct number of parameters.
+  # Examples: 
+  # tri_probit_vec2param(param = seq(3), 'mu_only')
+  # tri_probit_vec2param(param = seq(4), 'mu_const')
+  # tri_probit_vec2param(param = seq(6), 'mu_cov')
+  # 
+  
+  
+  # Extract vector of mean intercept.
+  mu <- param[1:3]
+  if (model_name == 'mu_only') {
+    if (length(param) != 3) {
+      stop("param not correct length.")
+    }
+    param <- NULL
+  } else {
+    param <- param[4:length(param)]
+  }
+  
+  
+  # Initialize covariance matrix. 
   Sigma = diag(c(1,1,1))
   
-  # Append the lower triangle of Sigma. 
-  Sigma[lower.tri(Sigma, diag = FALSE)] <- param
+  if (model_name == 'mu_only') {
+    # No additional parameters, Sigma is the identity matrix. 
+  } else if (model_name == 'mu_const') {
+    # The lower triangle of Sigma is a constant.
+    if (length(param) != 1) {
+      stop("param not correct length.")
+    }
+    Sigma[lower.tri(Sigma, diag = FALSE)] <- param
+  } else if (model_name == 'mu_cov') {
+    # The entire covariance matrix is specified.
+    if (length(param) != 3) {
+      stop("param not correct length.")
+    }
+    # Append the lower triangle of Sigma. 
+    Sigma[lower.tri(Sigma, diag = FALSE)] <- param
+  }
   
-  # Append the upper triangle of Sigma. 
-  Sigma[upper.tri(Sigma, diag = FALSE)] <- param
+  # Append the upper triangle of Sigma, imposing symmetry. 
+  Sigma[upper.tri(Sigma, diag = FALSE)] <- 
+    Sigma[lower.tri(Sigma, diag = FALSE)]
   
   return(list(mu = mu, Sigma = Sigma))
 }
@@ -152,9 +212,10 @@ tri_probit_vec2param <- function(param) {
 # Covariance matrix must be positive definite.
 # param_list <- tri_probit_vec2param(param = c(0,0,0, 0.001,0,0))
 # chol(param_list$Sigma)
+# For now, prime the optimization with a warm start. 
 
 
-tri_probit_loglike <- function(param, y) {
+tri_probit_loglike <- function(param, y, model_name) {
   
   # Likelihood function for a trivariate probit model. 
   # Examples:
@@ -166,7 +227,7 @@ tri_probit_loglike <- function(param, y) {
   
   # Get parameters. 
   n_cases <- nrow(y)
-  param_list <- tri_probit_vec2param(param)
+  param_list <- tri_probit_vec2param(param, model_name)
   
   # Verify that the covariance matrix is positive definite. 
   # Not necessary (yet).
@@ -208,7 +269,7 @@ tri_probit_loglike <- function(param, y) {
 
 
 
-tri_probit_estn <- function(y, param_0 = FALSE, est_hessian = FALSE) {
+tri_probit_estn <- function(y, model_name, param_0 = NULL, est_hessian = FALSE) {
   
   # Estimates the mean vector and correlation matrix 
   # for the trivariate probit model. 
@@ -217,15 +278,25 @@ tri_probit_estn <- function(y, param_0 = FALSE, est_hessian = FALSE) {
   # tri_probit_estn(tri_probit_gen(mu = c(1, 0, -1), Sigma = diag(c(1, 1, 1)), n_cases = 100))
   # tri_probit_estn(tri_probit_gen(mu = c(0, 0, 0), Sigma = diag(c(1, 1, 1)), n_cases = 100))
   
-  if (length(param_0) == 6) {
-    param_hat_0 <- param_0
+  # Need logic for model_name. 
+  # Could write function for number of parameters. 
+  num_params <- tri_probit_num_params(model_name)
+  if (!is.null(param_0)) {
+    if (length(param_0) == num_params) {
+      param_hat_0 <- param_0
+    } else {
+      stop("param not correct length.")
+    }
   } else {
-    param_hat_0 <- rep(0, 6)
+    param_hat_0 <- rep(0, num_params)
   }
+  # param_hat_0 <- param_0
+  # print(param_hat_0)
   
   tri_probit_optim <- optim(par = param_hat_0, 
                             fn = tri_probit_loglike, 
                             y = y, 
+                            model_name = model_name, 
                             method = "BFGS", 
                             hessian = est_hessian, 
                             control = list(fnscale= -1))
@@ -238,20 +309,16 @@ tri_probit_estn <- function(y, param_0 = FALSE, est_hessian = FALSE) {
   }
   
   param_hat <- tri_probit_optim$par
-  param_list <- tri_probit_vec2param(param_hat)
+  param_list <- tri_probit_vec2param(param_hat, model_name)
   
   
   
   # If Hessian matrix is returned, append it to the vector of outputs. 
+  estn_list <- list(param_hat = param_hat, 
+                    mu_hat = param_list$mu, 
+                    Sigma_hat = param_list$Sigma)
   if (est_hessian) {
-    estn_list <- list(param_hat = param_hat, 
-                       mu_hat = param_list$mu, 
-                       Sigma_hat = param_list$Sigma, 
-                       hessian = tri_probit_optim$hessian)
-  } else {
-    estn_list <- list(param_hat = param_hat, 
-                       mu_hat = param_list$mu, 
-                       Sigma_hat = param_list$Sigma)
+    estn_list$hessian <- tri_probit_optim$hessian
   }
   
   return(estn_list)
@@ -268,23 +335,53 @@ tri_probit_estn <- function(y, param_0 = FALSE, est_hessian = FALSE) {
 num_reps <- 100
 
 # Number of appeals court cases.
-n_cases <- 1000
+n_cases <- 100
 
 # Average intents of three appeals court judges.
 mu_0 <- c(1, 0, -1)
 
-# Covariance matrix of taste shifters. 
-Sigma_0 <- matrix(c(1.0, 0.8, 0.2, 
-                    0.8, 1.0, 0.5,
-                    0.2, 0.5, 1.0), ncol = 3)
 
-# Collect into a vector of parameters.
-param_0 <- tri_probit_param2vec(mu = mu_0, Sigma = Sigma_0)
+# Design of sigma depends on the chosen model. 
+# model_name <- 'mu_only'
+# param_0 <- mu_0
+
+model_name <- 'mu_const'
+param_0 <- c(mu_0, 0.5)
+# 
+# model_name <- 'mu_cov'
+# param_0 <- c(mu_0, 0.8, 0.2, 0.5)
 
 
-estn_results <- data.frame(matrix(nrow = num_reps, ncol = 6))
-colnames(estn_results) <- c(sprintf("mu_%d", seq(3)), 
-                            "Sigma_21", "Sigma_31", "Sigma_32")
+param_list <- tri_probit_vec2param(param_0, model_name)
+
+mu_0 <- param_list$mu
+Sigma_0 <- param_list$Sigma
+
+
+
+# Previous approach specified the parameters directly. 
+
+# # Covariance matrix of taste shifters. 
+# Sigma_0 <- matrix(c(1.0, 0.8, 0.2, 
+#                     0.8, 1.0, 0.5,
+#                     0.2, 0.5, 1.0), ncol = 3)
+# 
+# # Collect into a vector of parameters.
+# param_0 <- tri_probit_param2vec(mu = mu_0, Sigma = Sigma_0, model_name)
+
+
+if (model_name == 'mu_only') {
+  estn_results <- data.frame(matrix(nrow = num_reps, ncol = 3))
+  colnames(estn_results) <- c(sprintf("mu_%d", seq(3)))
+} else if (model_name == 'mu_const') {
+  estn_results <- data.frame(matrix(nrow = num_reps, ncol = 4))
+  colnames(estn_results) <- c(sprintf("mu_%d", seq(3)), 
+                              "Sigma_21")
+} else if (model_name == 'mu_cov') {
+  estn_results <- data.frame(matrix(nrow = num_reps, ncol = 6))
+  colnames(estn_results) <- c(sprintf("mu_%d", seq(3)), 
+                              "Sigma_21", "Sigma_31", "Sigma_32")
+}
 
 
 
@@ -302,7 +399,7 @@ for (rep_num in 1:num_reps) {
   y_sim <- tri_probit_gen(mu = mu_0, Sigma = Sigma_0, n_cases = n_cases)
   
   # Estimate model.
-  estn_list <- tri_probit_estn(y = y_sim, param_0 = param_0)
+  estn_list <- tri_probit_estn(y = y_sim, model_name = model_name, param_0 = param_0)
   
   # Store estimation results. 
   estn_results[rep_num, ] <- estn_list$param_hat
@@ -315,44 +412,75 @@ summary(estn_results)
 
 # I ran a simulation with these parameter inputs:
 
+
 # # Number of replications for simulation of estimation. 
 # num_reps <- 100
 # 
 # # Number of appeals court cases.
-# n_cases <- 1000
-# 
+# n_cases <- 100
 # 
 # # Average intents of three appeals court judges.
 # mu_0 <- c(1, 0, -1)
 # 
-# # Covariance matrix of taste shifters. 
-# Sigma_0 <- matrix(c(1.0, 0.8, 0.2, 
-#                     0.8, 1.0, 0.5,
-#                     0.2, 0.5, 1.0), ncol = 3)
-
-# It took a long time but these are some preliminary results:
-
-# mu_1             mu_2               mu_3        
-# Min.   :0.8618   Min.   :-0.09456   Min.   :-1.0914  
-# 1st Qu.:0.9738   1st Qu.:-0.01810   1st Qu.:-1.0517  
-# Median :0.9924   Median : 0.00724   Median :-1.0038  
-# Mean   :0.9922   Mean   : 0.00679   Mean   :-1.0063  
-# 3rd Qu.:1.0256   3rd Qu.: 0.03965   3rd Qu.:-0.9744  
-# Max.   :1.0646   Max.   : 0.07614   Max.   :-0.8935  
-# NA's   :65       NA's   :65         NA's   :65       
-#     Sigma_21         Sigma_31          Sigma_32     
-#  Min.   :0.7614   Min.   :0.02531   Min.   :0.3488  
-#  1st Qu.:0.7931   1st Qu.:0.14109   1st Qu.:0.4544  
-#  Median :0.8102   Median :0.20491   Median :0.4822  
-#  Mean   :0.8118   Mean   :0.19694   Mean   :0.4899  
-#  3rd Qu.:0.8299   3rd Qu.:0.24557   3rd Qu.:0.5265  
-#  Max.   :0.8919   Max.   :0.35464   Max.   :0.5835  
-#  NA's   :65       NA's   :65        NA's   :65   
+# 
+# # Design of sigma depends on the chosen model. 
+# model_name <- 'mu_only'
+# param_0 <- mu_0
+# 
+# > Sigma_0
+# [,1] [,2] [,3]
+# [1,]    1    0    0
+# [2,]    0    1    0
+# [3,]    0    0    1
 
 
-# The mean is easier to estimate than the covariance. 
-# In the next model, I will place more restrictions 
-# on the covariance matrix. 
+# These were the results:
+
+# mu_1             mu_2                 mu_3        
+# Min.   :0.6433   Min.   :-2.275e-01   Min.   :-1.4758  
+# 1st Qu.:0.9154   1st Qu.:-7.527e-02   1st Qu.:-1.1264  
+# Median :0.9945   Median :-2.900e-07   Median :-0.9945  
+# Mean   :1.0242   Mean   :-1.436e-03   Mean   :-1.0142  
+# 3rd Qu.:1.1264   3rd Qu.: 7.527e-02   3rd Qu.:-0.9154  
+# Max.   :1.4758   Max.   : 3.055e-01   Max.   :-0.5244  
+
+
+# It works much better with an identity restriction on the 
+# covariance matrix but 100 observations does not buy us much accuracy. 
+
+# At least estimating the mean equation is numerically stable
+# with only 100 observations. 
+
+
+
+
+# Next, I ran another simulation with these parameter inputs:
+
+# # Number of replications for simulation of estimation. 
+# num_reps <- 100
+# 
+# # Number of appeals court cases.
+# n_cases <- 100
+# 
+# # Average intents of three appeals court judges.
+# mu_0 <- c(1, 0, -1)
+# 
+# 
+# 
+# model_name <- 'mu_const'
+# param_0 <- c(mu_0, 0.5)
+
+# > Sigma_0
+# [,1] [,2] [,3]
+# [1,]  1.0  0.5  0.5
+# [2,]  0.5  1.0  0.5
+# [3,]  0.5  0.5  1.0
+
+
+
+# These were the results:
+
+
 
 
 ##################################################
