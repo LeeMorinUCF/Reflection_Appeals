74fa9fac113bf489ccbd6b7e8bc59e6847c40cab
Lee Morin <42477710+LeeMorinUCF@users.noreply.github.com>
2021-05-29 01:58:05 -0400

    Reading header of txt file for first case

--- prelim_reads/Read_Cases1.py
+++ prelim_reads/Read_Cases1.py
@@ -40,6 +40,9 @@ from lxml import etree
 # Modules for reading from doc (Word 97 2003):
 import win32com
 
+# Module for detecting type of character.
+import chardet
+
 
 ##################################################
 # Set Working Directory.
@@ -106,6 +109,24 @@ def doc2txt_file(app, doc_file, txt_file):
 
 
 
+# Function to determine if a line contains the case code.
+def is_case_code(line):
+    # Case numbers have digits at the beginning and end of the line.
+    line_list = line.split()
+    if len(line) > 1:
+        
+        if line_list[0].isdigit() and line_list[-1].isdigit():
+            return True
+        else:
+            return False
+
+# Determine whether line goes beyond list of holdings.
+def is_finished_holdings(line):
+    if line[0] == '[' or line[0].strip() == '':
+        return False
+    else:
+        return True
+        
 
 ##################################################
 # Select a File and Scrape Contents
@@ -136,10 +157,154 @@ doc2txt_file(app, doc_file, txt_file)
 # Read through file and parse data
 ##################################################
 
-with open(txt_file, 'r') as file:
-    for line_num in range(10):
+
+
+
+# with open(txt_file, 'r', encoding = 'utf-16') as file:
+#     for line_num in range(20):
+#         line = file.readline()
+#         print(line)
+#         print(str(line))
+#         print(line.rstrip())
+#         # print(line.decode('utf-16'))
+#         print(line.split())
+#         print(str(line).rstrip().split())
+#         print(line.replace("\r\n","").split())
+
+
+
+
+import glob
+
+# Get list of all files in a given directory sorted by name
+txt_file_list = sorted( filter( os.path.isfile,
+                        glob.glob(txt_path + '*') ) )
+
+
+# Choose a file and read the case information. 
+txt_file_num = 1
+
+# Read the information from this case.
+txt_file = txt_file_list[txt_file_num]
+
+print("Reading case information from file:")
+print(txt_file)
+print("")
+
+
+lines_read = 0
+with open(txt_file, 'r', encoding = 'utf-16') as file:
+    
+    # First look for the case number.
+    found_case_code = False
+    # while not found_case_num and lines_read < 10:
+    while not found_case_code:
+        # lines_read = lines_read + 1
         line = file.readline()
-        print(line)
+        # print(line)
+        
+        found_case_code = is_case_code(line)
+        
+        # line_bytes = line_bytes[0:(len(line_bytes) - 1)]
+        # line_bytes = line_bytes.rstrip()
+        # line_str = line_bytes.decode('utf-16')
+        # found_case_num = is_case_num(line_str)
+    
+    # Record the case number. 
+    case_code = line.replace("\n","")
+    
+    # The next line should be "United States Court of Appeals,"
+    # lines_read = lines_read + 1
+    line = file.readline()
+    # This line should be the circuit number.
+    line = file.readline()
+    circ_num = line.split()[0]
+    
+    # The next name should be the Plaintiff-Appellant.
+    line = file.readline()
+    pla_appnt_list = line.split()
+    pla_appnt = ' '.join(pla_appnt_list[0:len(pla_appnt_list) - 1])
+    pla_appnt = pla_appnt.replace(",","")
+    
+    # The next line is "v."
+    line = file.readline()
+    
+    # The next name should be the Defendant-Appellee.
+    line = file.readline()
+    def_appee_list = line.split()
+    def_appee = ' '.join(def_appee_list[0:len(def_appee_list) - 1])
+    def_appee = def_appee.replace(",","")
+    
+    # The next line should be the case number.
+    line = file.readline()
+    case_num = line.replace("\n","")
+    
+    # The next line is a pipe (|).
+    line = file.readline()
+    
+    # The next line is the date in text format.
+    line = file.readline()
+    case_date = line.replace("\n","")
+    
+    # The next line is the header "Synopsis".
+    line = file.readline()
+    
+    # The next line is the "Background" paragraph.
+    line = file.readline()
+    background = line.replace("\n","")
+    
+    # The next line is the "Holdings" header statement.
+    line = file.readline() # Skip a blank line.
+    line = file.readline()
+    holdings_hdr = line.replace("\n","")
+    
+    finished_holdings = False
+    while not finished_holdings:
+        line = file.readline()
+        finished_holdings = is_finished_holdings(line)
+        
+    # Record the case outcome. 
+    outcome = line.replace("\n","")
+    
+    # The next line is the statement of "Procedural Posture(s)".
+    line = file.readline() # Skip a blank line.
+    line = file.readline()
+    posture = line.replace("\n","")
+
+
+# Print the results.
+print("case_code = ")
+print(case_code)
+
+print("circ_num = ")
+print(circ_num)
+
+print("pla_appnt = ")
+print(pla_appnt)
+
+print("def_appee = ")
+print(def_appee)
+
+print("case_num = ")
+print(case_num)
+
+print("case_date = ")
+print(case_date)
+
+print("background = ")
+print(background)
+
+print("holdings_hdr = ")
+print(holdings_hdr)
+
+print("outcome = ")
+print(outcome)
+
+print("posture = ")
+print(posture)
+
+
+
 
 
 
